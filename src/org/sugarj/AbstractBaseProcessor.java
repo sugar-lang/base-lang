2f7da96a79d1af57c84cfce5d70f1ddc2b22d2ea
Sebastian Erdweg <seba.github@gmail.com>
2014-02-13 09:50:16 +0100

    cleardep management

--- src/org/sugarj/AbstractBaseProcessor.java
+++ src/org/sugarj/AbstractBaseProcessor.java
@@ -3,8 +3,11 @@ package org.sugarj;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.spoofax.interpreter.terms.IStrategoTerm;
 import org.strategoxt.HybridInterpreter;
@@ -13,7 +16,6 @@ import org.sugarj.common.Environment;
 import org.sugarj.common.FileCommands;
 import org.sugarj.common.errors.SourceCodeException;
 import org.sugarj.common.path.Path;
-import org.sugarj.util.Pair;
 
 public abstract class AbstractBaseProcessor implements IBaseProcessor, Serializable {
 
@@ -38,39 +40,42 @@ public abstract class AbstractBaseProcessor implements IBaseProcessor, Serializa
 	
 	// Returns true is files were generated or if there were no files to generate
 	// Returns false if generation was skipped due to generateFiles being false
-	public void compile(
+	public Set<Path> compile(
 	    Path outFile, 
 	    String source, 
 	    Path bin,
 	    List<Path> path,
-			Map<Path, Pair<Path, String>> deferredSourceFilesForSourceFile,
-			Map<Path, Integer> generatedFileHashes
+			Map<Path, String> deferredSourceFilesForSourceFile
 			) throws IOException, ClassNotFoundException, SourceCodeException {
 	  
 	  
-    List<Path> outFiles = new ArrayList<Path>();
+    List<Path> outSourceFiles = new ArrayList<>();
+    Set<Path> generatedFiles = new HashSet<>();
 
-    for (Pair<Path, String> deferredSource : deferredSourceFilesForSourceFile.values()) {
-      String code = deferredSource.b;
-      writeToFile(generatedFileHashes, deferredSource.a, code);
-      outFiles.add(deferredSource.a);
+    for (Entry<Path, String> deferredSource : deferredSourceFilesForSourceFile.entrySet()) {
+      String code = deferredSource.getValue();
+      
+      writeToFile(generatedFiles, deferredSource.getKey(), code);
+      outSourceFiles.add(deferredSource.getKey());
     }
 
     if (!source.isEmpty()) {
-      writeToFile(generatedFileHashes, outFile, source);
-      outFiles.add(outFile);
+      writeToFile(generatedFiles, outFile, source);
+      outSourceFiles.add(outFile);
     }
     
-    if (!outFiles.isEmpty()) {
-      List<Path> generatedByCompiler = compile(outFiles, bin, path);
+    if (!outSourceFiles.isEmpty()) {
+      List<Path> generatedByCompiler = compile(outSourceFiles, bin, path);
       for (Path p : generatedByCompiler)
-        generatedFileHashes.put(p, FileCommands.fileHash(p));
+        generatedFiles.add(p);
     }
+    
+    return generatedFiles;
  	}
 
-  private void writeToFile(Map<Path, Integer> generatedFileHashes, Path file, String content) throws IOException { 
+  private void writeToFile(Set<Path> generatedFiles, Path file, String content) throws IOException { 
 		FileCommands.writeToFile(file, content);
-		generatedFileHashes.put(file, FileCommands.fileHash(file));
+		generatedFiles.add(file);
 	}
 
   public          String getImportLocalName(IStrategoTerm decl) { return null; }
