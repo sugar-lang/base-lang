c9d1fd1235e016075c179be850f77eacddfb983b
Sebastian Erdweg <seba.github@gmail.com>
2014-02-15 16:04:58 +0100

    fix delegate

--- src/org/sugarj/AbstractBaseProcessor.java
+++ src/org/sugarj/AbstractBaseProcessor.java
@@ -5,8 +5,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.spoofax.interpreter.terms.IStrategoTerm;
@@ -14,6 +12,7 @@ import org.strategoxt.HybridInterpreter;
 import org.sugarj.common.ATermCommands;
 import org.sugarj.common.Environment;
 import org.sugarj.common.FileCommands;
+import org.sugarj.common.Log;
 import org.sugarj.common.errors.SourceCodeException;
 import org.sugarj.common.path.Path;
 
@@ -45,26 +44,22 @@ public abstract class AbstractBaseProcessor implements IBaseProcessor, Serializa
 	    String source, 
 	    Path bin,
 	    List<Path> path,
-			Map<Path, String> deferredSourceFilesForSourceFile
+			Set<Path> deferredSourceFilesForSourceFile
 			) throws IOException, ClassNotFoundException, SourceCodeException {
 	  
 	  
     List<Path> outSourceFiles = new ArrayList<>();
     Set<Path> generatedFiles = new HashSet<>();
 
-    for (Entry<Path, String> deferredSource : deferredSourceFilesForSourceFile.entrySet()) {
-      String code = deferredSource.getValue();
-      
-      writeToFile(generatedFiles, deferredSource.getKey(), code);
-      outSourceFiles.add(deferredSource.getKey());
-    }
-
+    outSourceFiles.addAll(deferredSourceFilesForSourceFile);
+    
     if (!source.isEmpty()) {
       writeToFile(generatedFiles, outFile, source);
       outSourceFiles.add(outFile);
     }
     
     if (!outSourceFiles.isEmpty()) {
+      Log.log.log("Compile desugared source files " + outSourceFiles, Log.CORE);
       List<Path> generatedByCompiler = compile(outSourceFiles, bin, path);
       for (Path p : generatedByCompiler)
         generatedFiles.add(p);
